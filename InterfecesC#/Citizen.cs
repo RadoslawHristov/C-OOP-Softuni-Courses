721eb0f3c734ff722a9891edc0a7a0429fd49252
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-01-21 12:21:43 +0200

    Add files via upload

---
+++ InterfecesC#/Citizen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson,IResident
+    {
+        public Citizen(string name, string country, int age)
+        {
+            Name = name;
+            Country = country;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        string IResident.GetName()
+        {
+            return $"{"Mr/Ms/Mrs "}{Name}";
+        }
+
+        public int Age { get; set; }
+        string IPerson.GetName()
+        {
+            return $"{Name}";
+        }
+    }
+}
