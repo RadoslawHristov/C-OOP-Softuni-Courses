2f8fdc4c86d7be52a23de91e4a425c05c1a69277
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-02-03 20:24:33 +0200

    Add files via upload

---
+++ PersonClass/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            Team teams = new Team("radi");
+            for (int i = 0; i < count; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var peoples = new Person(input[0], input[1], int.Parse(input[2]),decimal.Parse(input[3]));
+                teams.AddPlayer(peoples);
+            }
+
+            Console.WriteLine($"First team has {teams.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {teams.ReserveTeam.Count} players.");
+        }
+    }
+}
