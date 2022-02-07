f6276b4576e162a28c68650b1e312ce9a4bfb194
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-02-07 12:44:20 +0200

    Add files via upload

---
+++ Mathoperation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            Console.WriteLine(math.Add(1, 5));
+            Console.WriteLine(math.Add(1.2m,3.4m,4.9m));
+            Console.WriteLine(math.Add(1.4,3.1,4.1));
+        }
+    }
+}
