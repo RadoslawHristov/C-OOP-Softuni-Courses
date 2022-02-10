9a0a39f922c1532ff40b5cec261e806582d52ef4
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-02-10 12:22:04 +0200

    Add files via upload

---
+++ ReflectionC#/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Niki")]
+    public class StartUp
+    {
+        [Author("Radi")]
+        public static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
