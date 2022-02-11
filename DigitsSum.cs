4ba6a09db8169a9ae14104fadfb5aca9b098b93c
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-02-11 12:46:52 +0200

    Add files via upload

---
+++ DigitsSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingDemo
+{
+    public class DigitsSum
+    {
+        public DigitsSum(int firstNumber, int secondNumber)
+        {
+            FirstNumber = firstNumber;
+            SecondNumber = secondNumber;
+        }
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+
+        public int ValidSum(int first,int secont)
+        {
+            var result = first + secont;
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
-namespace Test
+namespace TestingDemo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             
         }
