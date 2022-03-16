345247e11acdc2197c48b7d92d97403dc167d6cf
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-16 12:32:33 +0200

    Add files via upload

---
+++ homeWorks/Validator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Vehicles
+{
+    public class Validator
+    {
+        public static void CheckPositiveNumber(double value)
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Fuel must be a positive number");
+            }
+        }
+    }
+}
