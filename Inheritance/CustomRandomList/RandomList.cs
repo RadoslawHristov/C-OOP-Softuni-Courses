15f24302a2b82551fdbfaf84449fb5604cee9462
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-20 21:15:24 +0200

    Add files via upload

---
+++ Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+        private List<string> listy = new List<string>();
+
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+        public string RandomString()
+        {
+            int index = rnd.Next(0, listy.Count);
+            string str = listy[index].ToString();
+            return str;
+        }
+
+
+        public void Add(string item)
+        {
+            listy.Add(item);
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(string item)
+        {
+            return listy.Contains(item);
+        }
+
+        public void CopyTo(string[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string item)
+        {
+            return listy.Remove(item);
+        }
+
+        public int Count => listy.Count;
+    }
+}
---
+++ Inheritance/CustomRandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            list.Add("Radi");
+            list.Add("Maya");
+            list.Add("lelia");
+            Console.WriteLine(list.RandomString());
+            
+        }
+    }
+}
