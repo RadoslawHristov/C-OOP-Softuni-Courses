3c2e095d6d9d045e0cefac1ce09928d52247122c
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-20 21:18:54 +0200

    Add files via upload

---
+++ Inheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_of_Strings
+{
+    public class StackOfStrings : Stack
+    {
+        private Stack<string> stack = new Stack<string>();
+        public bool IsEmpty()
+        {
+            if (stack.Count==0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public Stack<string> AddRange(Stack<string> stacks)
+        {
+            foreach (var item in stacks)
+            {
+                this.stack.Push(item);
+            }
+            return stack;
+        }
+    }
+}
---
+++ Inheritance/CustomStack/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack_of_Strings
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Stack<string> input = new Stack<string>();
+            input.Push("Radi");
+            input.Push("Maya");
+            input.Push("Paco");
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(input);
+            Console.WriteLine(stack.IsEmpty());
+            
+        }
+    }
+}
