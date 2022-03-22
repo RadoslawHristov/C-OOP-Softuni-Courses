c9ebefcb4daaf124f3f21709b8a0ddc6472bd119
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-22 12:33:17 +0200

    Add files via upload

---
+++ BladeKnight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlayersandMonsters;
+
+namespace PlayersAndMonsters
+{
+    public class BladeKnight: DarkKnight
+    {
+        public BladeKnight(string username, int level)
+            : base(username, level)
+        {
+        }
+    }
+}
---
+++ DarkWizard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersandMonsters
+{
+    public class DarkWizard:Wizard
+    {
+        public DarkWizard(string username, int level) 
+            : base(username, level)
+        {
+        }
+    }
+}
