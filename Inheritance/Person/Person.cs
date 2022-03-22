3506805367e100f89411f5c539ec9f2e49e47da8
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-22 12:31:27 +0200

    Add files via upload

---
+++ Inheritance/Person/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (age < 0)
+                {
+                    throw new Exception("Age not be negative");
+                }
+                age = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                this.Name,
+                this.Age));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Inheritance/Person/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Person person = null;
+            Child child = null;
+
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age > 15)
+            {
+                person = new Person(name,age);
+                Console.WriteLine(person.ToString());
+            }
+            else if (age <= 15)
+            {
+                child = new Child(name,age);
+                Console.WriteLine(child.ToString());
+            }
+        }
+    }
+}
