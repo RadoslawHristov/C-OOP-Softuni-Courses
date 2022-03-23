8702c3609f434e9b2980e09a28bf68d86d026b09
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-23 12:28:46 +0200

    Add files via upload

---
+++ Inheritance/Animal/Animal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        protected string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        protected int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value.ToString()) || string.IsNullOrWhiteSpace(value.ToString()) || value <= 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                this.age = value;
+            }
+        }
+
+        protected string Gender
+        {
+            get { return this.gender; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.gender = value;
+            }
+        }
+
+        public virtual string ProduceSound()
+        {
+            return "Not implemented!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{this.name} {this.age} {this.gender}");
+            sb.Append(Environment.NewLine);
+            sb.Append(ProduceSound());
+            return sb.ToString();
+        }
+    }
+}
