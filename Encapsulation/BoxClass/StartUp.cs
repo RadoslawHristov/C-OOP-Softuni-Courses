36fb090cf0484884a6f2baad0611d7d9124d327f
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-25 12:30:42 +0200

    Add files via upload

---
+++ Encapsulation/BoxClass/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Class_Box_Data
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(lenght,width,height);
+
+            Console.WriteLine($"Surface Area - {box.ReturnArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
+        }
+    }
+}
