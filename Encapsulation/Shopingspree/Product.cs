a8e045f3dbef0c2fe9e20f4c5e438fff1ff4a262
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-28 12:24:58 +0300

    Add files via upload

---
+++ Encapsulation/Shopingspree/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+
+        private string name;
+        private double cost;
+
+        public Product(string name, double cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+        public double Cost
+        {
+            get
+            {
+                return cost;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be a negative number.");
+                }
+                cost = value;
+            }
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("The name cannot be an empty string");
+                }
+                name = value;
+            }
+        }
+    }
+}
