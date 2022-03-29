01409d47154d7f11a5ac96f98047e7c8823a2f6f
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-29 12:35:00 +0300

    Add files via upload

---
+++ AbstractClassAndInterfeces/drawCircle/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radios)
+        {
+            Radius = radios;
+        }
+
+
+        public int Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+
+        public void Draw()
+        {
+            double radIn = this.Radius - 0.4;
+            double radOut = this.Radius + 0.4;
+
+            for (double y = this.Radius; y >= -this.Radius; y--)
+            {
+                for (double x = -this.Radius; x < radOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= radIn * radIn && value <= radOut * radOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
