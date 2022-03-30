b27d20bbad46ad409dab8a32cdfb41b171dd76a7
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-03-30 12:34:26 +0300

    Add files via upload

---
+++ AbstractClassAndInterfeces/BirthDayTask/Pet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BorderControl;
+
+namespace BirthdayCelebrations
+{
+    public class Pet: IWhoIAm,IBirthsDay
+    {
+        private string name;
+        private string birthdaydate;
+
+        public Pet(string name, string birthdaydate)
+        {
+            this.Name = name;
+            this.Birthday = birthdaydate;
+        }
+        public string Name
+        {
+            get => name;
+            set => name=value;
+        }
+
+        public string Id
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public string Birthday
+        {
+            get => birthdaydate;
+            set => birthdaydate=value;
+        }
+    }
+}
---
+++ AbstractClassAndInterfeces/BirthDayTask/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using BirthdayCelebrations;
+
+namespace BorderControl
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            IWhoIAm peotrsonOrrob = null;
+            IBirthsDay day= null;
+            List<IWhoIAm> persons = new List<IWhoIAm>();
+
+            List<IBirthsDay> birthdays = new List<IBirthsDay>();
+
+            while (input != "End")
+            {
+                string[] splited = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string comand = splited[0];
+                if (comand== "Citizen")
+                {
+                    string name = splited[1];
+                    int age = int.Parse(splited[2]);
+                    string id = splited[3];
+                    string birthday = splited[4];
+                    peotrsonOrrob = new Citizens(name,age,id,birthday);
+                    persons.Add(peotrsonOrrob);
+                    day = new Citizens(name,age,id, birthday);
+                    birthdays.Add(day);
+                }
+                else if (comand=="Robot")
+                {
+                    string model = splited[1];
+                    string id = splited[2];
+                    peotrsonOrrob = new Robots(model,id);
+                    persons.Add(peotrsonOrrob);
+                }
+                else if (comand== "Pet")
+                {
+                    string name = splited[1];
+                    string birthday = splited[2];
+                    peotrsonOrrob = new Pet(name,birthday);
+                    persons.Add(peotrsonOrrob);
+                    day = new Pet(name,birthday);
+                    birthdays.Add(day);
+                }
+                input = Console.ReadLine();
+            }
+
+            string lastdigitId = Console.ReadLine();
+
+            foreach (var days in birthdays)
+            {
+                if (days.Birthday.EndsWith(lastdigitId))
+                {
+                    Console.WriteLine(days.Birthday);
+                }
+            }
+        }
+    }
+}
