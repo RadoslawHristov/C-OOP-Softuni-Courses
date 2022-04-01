b9bd1cc0d62ae1b6bc9ee6a751c5668fbb1fc152
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-04-01 20:46:57 +0300

    Add files via upload

---
+++ AbstractClassAndInterfeces/ExploicitInterfeces/IResident.cs
@@ -0,0 +1,10 @@
+﻿namespace ExplicitInterfaces
+{
+    public interface IResident
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public string GetName();
+    }
+}
