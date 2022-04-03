a02e4231f0c3823dabf14f82f593d4e5e45c8b39
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-04-03 17:14:42 +0300

    Add files via upload

---
+++ AbstractClassAndInterfeces/MilitaryElite/Model/Repair.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MilitaryElite.Interfeces;
+
+namespace MilitaryElite.Model
+{
+    public class Repair : IRepair
+    {
+        private string partName;
+        private int hoursWorked;
+
+        public Repair(string partName, int hoursWorked)
+        {
+            this.partName = partName;
+            this.hoursWorked = hoursWorked;
+        }
+
+        public string PartName { get; private set; }
+
+        public int HoursWorked { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Part Name: {this.partName} Hours Worked: {this.hoursWorked}";
+        }
+    }
+}
