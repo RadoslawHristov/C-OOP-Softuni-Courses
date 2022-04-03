d374fd49e6ad48a4a41e3e4e28a43a87dc7d8e76
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-04-03 17:13:55 +0300

    Add files via upload

---
+++ AbstractClassAndInterfeces/MilitaryElite/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MilitaryElite
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            var engine = new Engine();
+            engine.Run();
+        }
+    }
+}
