00ef5b22b6fbd3c0ea8228a081fd031c71febf75
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-04-05 12:34:59 +0300

    Add files via upload

---
+++ Polymorphism/Animals/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        protected Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+        public string FavouriteFood
+        {
+            get { return favouriteFood; }
+            set { favouriteFood = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}
