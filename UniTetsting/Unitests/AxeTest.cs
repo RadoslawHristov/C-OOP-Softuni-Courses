a49dc64e2fbac39bf141f457816ed23b21bafc25
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-04-10 21:49:10 +0300

    Add files via upload

---
+++ UniTetsting/Unitests/AxeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTest
+    {
+        [Test]
+        public void TestAxeAttackDurability()
+        {
+            Axe axe = new Axe(10,10);
+            Dummy dummy = new Dummy(10,10);
+
+            axe.Attack(dummy);
+
+            Assert.AreEqual(axe.DurabilityPoints,9);
+        }
+
+    }
+}
