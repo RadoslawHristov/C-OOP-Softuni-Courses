4403484315d0dc62e5a3c743259c10a229d043b7
Radoslav Hristov <81433482+RadoslawHristov@users.noreply.github.com>
2022-05-11 12:42:07 +0300

    Add files via upload

---
+++ ProjectGame/GameSnakeOfClasses/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using ProjectGameOfMatrix;
+using SimpleSnake.GameObjects;
+
+namespace SimpleSnake
+{
+    using Utilities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ConsoleWindow.CustomizeConsole();
+            Wall wall = new Wall(100 ,30);
+            Snake snake = new Snake(wall);
+
+            Engine engine = new Engine(wall,snake);
+            engine.Run();
+        }
+    }
+}
